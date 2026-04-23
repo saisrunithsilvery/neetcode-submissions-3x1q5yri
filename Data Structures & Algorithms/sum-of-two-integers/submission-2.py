c0bf4332e4bc45a38ca233add501ae780432137d
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-04-23 12:55:45 -0400

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+
+
+        while b!= 0 :
+
+            carry = (a & b) << 1
+            a = (a ^ b )& 0xFFFFFFFF 
+            b = carry
+
+        return a    
