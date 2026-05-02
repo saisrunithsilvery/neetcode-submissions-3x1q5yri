324232e08238c85f0944dd0df28c2b648bb7cfb4
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-05-02 11:36:49 -0400

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+
+        # memo = [[0]*(n+1) for _ in range(target)]
+        n = len(nums)
+        def solve(i, amt):
+
+            if i == n:
+                return 1 if amt == target else 0
+
+    
+            
+
+            x = solve( i+1, amt+nums[i])
+            y = solve(i+1, amt - nums[i])
+
+            return x+y
+        return solve(0, 0)    
+
