4788bee17d619434a12bcdcb561ad66995fdb326
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-05-02 21:38:41 -0400

    Add: count-subsequences - submission-8

---
+++ Data Structures & Algorithms/count-subsequences/submission-8.py
@@ -0,0 +1,33 @@
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n = len(s)
+        m = len(t)
+        
+        dp = [[0]*(m+1) for _ in range(0,n+1)]
+
+        def solve(i, j):
+
+             
+
+            if j == m:
+                return 1
+            
+            if i == n and j < m:
+                return 0   
+
+            if dp[i][j] != 0:
+                return dp[i][j]
+            x = 0
+            y = 0
+            if s[i]== t[j]:
+                x = solve(i+1, j+1)
+            y = solve(i+1, j)
+
+            dp[i][j] = x + y
+
+            return dp[i][j]
+
+        return solve(0,0)            
+
+                    
