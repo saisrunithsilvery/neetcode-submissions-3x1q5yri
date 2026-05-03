d9863b14c2c815e086e923634c3d467609acbc00
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-05-03 00:39:54 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses, prerequisites):
+        
+        premap = defaultdict(list)
+
+        for crs, pre in prerequisites:
+            premap[crs].append(pre)
+        visit = set()
+
+        def dfs(crs):
+
+            if crs in  visit :
+                return False 
+
+            if premap[crs] == []:
+                return True
+            visit.add(crs)    
+
+            for i in premap[crs]:
+
+                if not dfs(i):
+                    return False
+            visit.remove(crs)
+            premap[crs] =[]
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True        
+
+
