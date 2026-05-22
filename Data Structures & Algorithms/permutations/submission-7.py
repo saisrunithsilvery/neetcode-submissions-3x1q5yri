038a4b0226b15a59e36a78cc86e0945582fadd8c
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-05-22 09:11:08 -0400

    Add: permutations - submission-7

---
+++ Data Structures & Algorithms/permutations/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        i = 0
+        res =[]
+        def bt(i):
+
+            if i == len(nums):
+                res.append(nums[:])
+                return
+
+            j = i
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                bt(i+1)
+                nums[i],nums[j] = nums[j], nums[i]
+
+        bt(i)   
+        return res         
+
+
+
+
+
+        
