0447b43bb13e827de06d4bddcd1804bb94a862f7
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-06-05 14:20:32 -0400

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,25 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.list1 = [None]*(10**6 + 1)
+        
+
+    def put(self, key: int, value: int) -> None:
+        self.list1[key] = value
+        
+
+    def get(self, key: int) -> int:
+        return self.list1[key] if self.list1[key]!= None else -1
+        
+
+    def remove(self, key: int) -> None:
+
+        self.list1[key] = None
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
