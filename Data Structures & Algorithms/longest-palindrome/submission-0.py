311af7e5206320a9da568ac7856b08c521d60906
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-06-17 14:04:34 -0400

    Add: longest-palindrome - submission-0

---
+++ Data Structures & Algorithms/longest-palindrome/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hashmap = {}
+        count = 0
+
+        for i in s:
+            hashmap[i] = hashmap.get(i, 0)+1
+
+       
+
+        for i in hashmap.values():
+
+            if i%2 == 0:
+                count += i
+            else:
+                if i-1 != 0:
+                    count += i-1
+        return count+1               
+
