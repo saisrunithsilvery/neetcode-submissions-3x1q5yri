6a1f4c8a60f47bcf30ae74f05bc39c2e228ed28a
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-06-17 14:17:18 -0400

    Add: largest-substring-between-two-equal-characters - submission-0

---
+++ Data Structures & Algorithms/largest-substring-between-two-equal-characters/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        hashmap = {}
+        longest = -1
+
+        for i, char in enumerate(s):
+
+            if char not in hashmap:
+                hashmap[char] = i
+            else:
+
+                longest = i- hashmap[char] -1
+
+        return longest        
+
+
+
+
+
+            
+
+        
