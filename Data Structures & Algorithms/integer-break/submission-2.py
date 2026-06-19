5ecc0fa6f50eb58697ddb5259ae7d37e2ee32727
saisrunithsilvery <156823776+saisrunithsilvery@users.noreply.github.com>
2026-06-19 10:16:18 -0400

    Add: integer-break - submission-2

---
+++ Data Structures & Algorithms/integer-break/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+       
+        dp = [0] * (n + 1)
+        
+        # Base case
+        dp[1] = 1  # Can't break 1, but set for safety
+        dp[2] = 1  # Only option: 1+1, product = 1
+        
+        for i in range(3, n + 1):
+            for j in range(1, i):
+                # Compare:
+                # - Keep (i-j) as-is: j * (i-j)
+                # - Break (i-j) further: j * dp[i-j]
+                dp[i] = max(dp[i], j * max(i - j, dp[i - j]))
+        
+        return dp[n]
+
+
+    
